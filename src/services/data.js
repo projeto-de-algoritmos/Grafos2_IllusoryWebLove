2753dbe7cc6209fc897df5d44676ee65a30c2031
Bruno-Felix <balvesfelix@gmail.com>
2022-07-10 16:06:33 -0300

    Inserindo cidades

---
+++ src/services/data.js
@@ -0,0 +1,30 @@
+export const data = [
+    {
+        "id": "1",
+        "name": "São Paulo",
+    },
+    {
+        "id": "2",
+        "name": "Espírito Santo",
+    },
+    {
+        "id": "3",
+        "name": "Minas Gerais",
+    },
+    {
+        "id": "4",
+        "name": "Rio de Janeiro",
+    },
+    {
+        "id": "5",
+        "name": "Santa Catarina",
+    },
+    {
+        "id": "6",
+        "name": "Paraná",
+    },
+    {
+        "id": "7",
+        "name": "Rio Grande do Sul",
+    }
+]
